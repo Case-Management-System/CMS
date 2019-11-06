c41e7f35eee9136180fa8dedce1ca14212eccbc4
user <user@Leon>
2019-11-07 00:07:22 +0800

    Merge Test

--- src/CaseManagementSystem.java
+++ src/CaseManagementSystem.java
@@ -13,6 +13,7 @@ public class CaseManagementSystem {
         control.setModel(model);
         control.setView(view);
         view.setController(control);
+        System.out.println("");
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 view.setVisible(true);
